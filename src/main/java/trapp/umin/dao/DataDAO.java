39dd85820e63bb876a31bc9685de613d0fb1c1fb
Artem Krumin <trapper.programmer@gmail.com>
2022-11-02 10:58:23 +0300

    error

--- src/main/java/trapp/umin/dao/DataDAO.java
+++ src/main/java/trapp/umin/dao/DataDAO.java
@@ -1,42 +1,49 @@
 package trapp.umin.dao;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import trapp.umin.models.Data;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class DataDAO {
-    private static final AtomicInteger ID=new AtomicInteger(0);
-    private JdbcTemplate jdbcTemplate;
+    //private final JdbcTemplate jdbcTemplate;
+    private final SessionFactory sessionFactory;
 
     @Autowired
-    public DataDAO(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate=jdbcTemplate;
+    public DataDAO(SessionFactory sessionFactory){
+      //  this.jdbcTemplate=jdbcTemplate;
+        this.sessionFactory=sessionFactory;
     }
 
+    @Transactional(readOnly = true)
     public List<Data> show(){
-        return jdbcTemplate.query("SELECT * FROM Data", new BeanPropertyRowMapper<>(Data.class));
+        Session session=sessionFactory.getCurrentSession();
+
+        List<Data> data=session.createQuery("SELECT d FROM Data d",Data.class).getResultList();
+
+        return data;
     }
 
     public Data index(int id){
-        return jdbcTemplate.query("SELECT * FROM Data WHERE id=?",new Object[]{id},new BeanPropertyRowMapper<>(Data.class)).stream().findAny().orElse(null);
+        return null;
+
     }
 
     public void create(Data data){
-        jdbcTemplate.update("INSERT INTO Data(name,surname,wallet,lock) VALUES (?,?,?,?)",data.getName(),data.getSurname(),data.getWallet(),data.getLock());
+
     }
 
     public void update(int id,Data data){
-        jdbcTemplate.update("UPDATE Data SET name=?,surname=?,wallet=?,lock=? WHERE id=?",data.getName(),data.getSurname(),data.getWallet(),data.getLock(),id);
+
     }
 
     public void delete(int id){
-        jdbcTemplate.update("DELETE FROM Data WHERE id=?",id);
     }
 
 }
--- src/main/java/trapp/umin/models/Data.java
+++ src/main/java/trapp/umin/models/Data.java
@@ -1,37 +1,26 @@
 package trapp.umin.models;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "data")
 public class Data {
 
+    @Id
+    @Column(name = "data_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotEmpty(message = "Name should be not empty")
-    @Size(min = 2,max = 20,message = "Name should be between 2 and 20 characters")
+    @Column(name = "name")
     private String name;
-    @NotEmpty(message = "Surname should be not empty")
-    @Size(min = 2,max = 20,message = "Surname should be between 2 and 20 characters")
-    private String surname;
-    @Min(value = 0, message = "Wallet should be more then 0")
-    private int wallet;
-    private String lock;
+    public Data(){}
 
     public void setId(int id){
         this.id=id;
     }
     public void setName(String name){
         this.name=name;
-    }
-    public void setSurname(String surname){
-        this.surname=surname;
-    }
-    public void setWallet(int wallet){
-        this.wallet=wallet;
-    }
-    public void setLock(String lock){
-        this.lock=lock;
     }
 
     public int getId(){
@@ -40,13 +29,5 @@ public class Data {
     public String getName(){
         return name;
     }
-    public String getSurname(){
-        return surname;
-    }
-    public int getWallet(){
-        return wallet;
-    }
-    public String getLock(){
-        return lock;
-    }
+
 }
